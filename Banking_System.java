4be02140b68b4215af6aa36a351dd80dc425ee8d
siva.s <155124939+siva-22-s@users.noreply.github.com>
2024-01-02 08:11:32 +0530

    java project

---
+++ Banking_System.java
@@ -0,0 +1,13 @@
+package com.BankingApplication;
+
+public class Banking_System {
+
+	public static void main(String[] args) {
+		
+		BankAccount obj = new BankAccount("siva","123ASE");
+		 
+		obj.ShowMenu();
+		
+	}
+
+}
